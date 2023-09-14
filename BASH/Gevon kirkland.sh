fff20b1da078693c79753ed472b5529c9b0ddeea
GevonKirkland <143045197+GevonKirkland@users.noreply.github.com>
2023-09-13 19:00:32 -0500

    first comait

--- BASH/Gevon kirkland.sh
+++ BASH/Gevon kirkland.sh
@@ -1,1 +1,8 @@
-
+#!/bin/sh
+echo "What do I Like to do in my free time"
+read "I like to watch football,play video games and make music"
+$MyVar+= "instreasting fact"
+$ MyVar " I have autsim"
+# ! /bin/sh
+if { "I was born in new jersey and raised in Kansas City"
+ls -l "I am very good at rembering stuff"
