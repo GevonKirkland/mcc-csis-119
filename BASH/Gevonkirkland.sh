1f9966d16037826b4091e03f6f65b0c20167e692
GevonKirkland <143045197+GevonKirkland@users.noreply.github.com>
2023-09-15 14:12:42 -0500

    Bash script

--- BASH/Gevonkirkland.sh
+++ BASH/Gevonkirkland.sh
@@ -1,1 +1,18 @@
-
+#!/bin/bash
+echo "Where was I borned an raised"
+read "I was born in New Jersey and i've been raised in Kansas City"
+echo game="mario"
+#!/bin/bash
+echo fav game="Sonic"
+#!/bin/bash
+echo if name=="mario"
+#!/bin/bash
+echo print("Fav game "sonic"
+#!/bin/bash
+echo if name=="Mario"
+#!/bin/bash
+echo print ("Wrong game tittle")
+#!/bin/bash
+echo grep "Fav genre of music" Rap.txt | Less
+#!/bin/bash
+echo count=5 "I have autism and ADHD while count to 5
