b5d88691da12908e3bf026cd1bb60559a5ae010b
GevonKirkland <143045197+GevonKirkland@users.noreply.github.com>
2023-09-25 23:25:33 -0500

    powershell script

--- Pwsh/GevonKirkland.ps1
+++ Pwsh/GevonKirkland.ps1
@@ -1,1 +1,11 @@
-
+echo "Where was I borned an raised"
+read-host "I was born in New Jersey and i've been raised in Kansas City"
+echo game="mario"
+echo fav game="Sonic"
+echo if name=="mario"
+Write-Host "Fav game ""sonic""
+if name=="Mario"
+Write-Output "Wrong game tittle"
+	Get-Content -Path "Rap.txt" | Select-String -Pattern "Fav genre of music" | Out-Host
+	$count = 5
+Write-Host "I have autism and ADHD while count to $count"
