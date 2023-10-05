779176fad63e71a17e47c97dcdb1e1e6a7f033c2
GevonKirkland <143045197+GevonKirkland@users.noreply.github.com>
2023-10-05 13:25:24 -0500

    powershell script

--- powershell/gevonkirkland.sh
+++ powershell/gevonkirkland.sh
@@ -1,1 +1,7 @@
-
+Write-Output "Where was I born and rasied"
+Write-Host "I was born in New Jersey and raised in KC"
+if (game==mario) {
+  Write-Host  "Right game sonic"
+}
+Write-Host #"what is my fav game"
+Write-Output "sonic"
