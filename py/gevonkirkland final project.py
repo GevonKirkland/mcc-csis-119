49c7c41a176bdaad9ea7f93ad16eade0573ef131
GevonKirkland <143045197+GevonKirkland@users.noreply.github.com>
2023-12-13 17:43:44 -0600

    final project.py

--- py/gevonkirkland final project.py
+++ py/gevonkirkland final project.py
@@ -1,1 +1,10 @@
-
+import requests
+first_name = input("First Name: ") 
+last_name = input("Last Name: ") 
+url = "https://api.chucknorris.io/jokes/random"
+params = {"limitTo" : "[nerdy]", 
+          "firstName" : first_name,
+          "lastname" : last_name}  
+response = requests.get(url)
+joke = response.json() ["value"]
+print(joke) 
